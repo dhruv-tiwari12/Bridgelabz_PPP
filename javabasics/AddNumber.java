ed1d9005889eed7c07419eb8e8dac2db4a78fb2c
dhruv-tiwari12 <tiwaridhruv151@gmail.com>
2025-08-11 11:59:33 +0530

    Add AddNumber.java in javabasics

---
+++ javabasics/AddNumber.java
@@ -0,0 +1,20 @@
+package Bridgelabz_PPP.javabasics;
+
+import java.util.Scanner;
+
+public class AddNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
+    }
+}
