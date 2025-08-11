28d5c5668251c9a79ac46cd9a19e5222a6f1c5ea
dhruv-tiwari12 <tiwaridhruv151@gmail.com>
2025-08-11 12:06:54 +0530

    Add Conversion.java in javabasics

---
+++ javabasics/Conversion.java
@@ -0,0 +1,12 @@
+package Bridgelabz_PPP.javabasics;
+public class Conversion {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static void main(String[] args) {
+        double celsius = 25;
+        double fahrenheit = celsiusToFahrenheit(celsius);
+        System.out.println(celsius + "°C is " + fahrenheit + "°F");
+    }
+}    
